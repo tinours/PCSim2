32d8a9547b1af92a47f1fdc866821cbf9f3dd832
David Bergeron <tinours@gmail.com>
2015-08-19 07:27:14 -0400

    Add Frame for EventQueueVisualizer

---
+++ src/com/cablelabs/gui/PC2EventQueueVisualizer.java
@@ -0,0 +1,12 @@
+package com.cablelabs.gui;
+
+import javax.swing.JFrame;
+
+public class PC2EventQueueVisualizer extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6066814708270182826L;
+
+}
